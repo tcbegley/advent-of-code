8ca9a9283f91fffbee3575d2461cb5c993b1d64a
tcbegley <tomcbegley@gmail.com>
2020-12-05 07:29:09 +0000

    2020 day 5

---
+++ 2020/day05.py
@@ -0,0 +1,17 @@
+import re
+import sys
+
+
+def answer(path):
+    with open(path) as f:
+        passes = re.sub(r"F|L", "0", f.read().strip())
+        passes = re.sub(r"B|R", "1", passes)
+        passes = passes.split("\n")
+
+    passes = [int(p, 2) for p in passes]
+
+    return max(passes)
+
+
+if __name__ == "__main__":
+    print(answer(sys.argv[1]))
