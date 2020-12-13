48c9fcce4444a5e8bfffe691f03eeb57d8139522
tcbegley <tomcbegley@gmail.com>
2020-12-13 11:43:18 +0000

    2020 day 13 attempt 2

--- 2020/day13.py
+++ 2020/day13.py
@@ -2,30 +2,6 @@ import sys
 from functools import reduce
 
 
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
-
-
-def lcm(a, b):
-    return a * b // gcd(a, b)
-
-
-def extended_gcd(a, b):
-    old_r, r = a, b
-    old_s, s = 1, 0
-    old_t, t = 0, 1
-
-    while r != 0:
-        quotient = old_r // r
-        old_r, r = r, old_r - quotient * r
-        old_s, s = s, old_s - quotient * s
-        old_t, t = t, old_t - quotient * t
-
-    return old_s, old_t
-
-
 def load_data(path):
     with open(path) as f:
         ts, buses = f.read().strip().split("\n")
@@ -44,20 +20,9 @@ def part_1(ts, buses):
 
 
 def part_2(buses):
-    def compare_2(bus1, bus2):
-        offset1, id1 = bus1
-        offset2, id2 = bus2
-
-        a, b = extended_gcd(id1, id2)
-        lcm_ = lcm(id1, id2)
-
-        t = (offset2 - offset1) * a * id1 + offset1
-
-        bus3 = (t % lcm_, lcm_)
-        return bus3
-
-    offset, period = reduce(compare_2, buses)
-    return period - offset
+    # application of chinese remainder theorem
+    N = reduce(lambda a, b: a * b, [b[1] for b in buses])
+    return sum(-o * (n_i := N // p) * pow(n_i, -1, p) for o, p in buses) % N
 
 
 if __name__ == "__main__":
