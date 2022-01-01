bdcea5f95cebb045af3f6c2aac7d2e04f99580e0
tcbegley <tomcbegley@gmail.com>
2022-01-01 18:19:49 +0000

    Tidy up 2018 day 2

--- 2018/day02.py
+++ 2018/day02.py
@@ -1,28 +1,38 @@
 import sys
-from collections import defaultdict
+from collections import Counter
+from itertools import combinations
 
 
-def letter_counts(code):
-    counts = defaultdict(lambda: 0)
-    for c in code:
-        counts[c] += 1
-    return dict(counts)
-
-
-def answer(path):
+def load_data(path):
     with open(path) as f:
-        codes = f.read().strip().split("\n")
+        return [line.strip() for line in f.readlines()]
+
+
+def part_1(codes):
     n2, n3 = 0, 0
 
     for code in codes:
-        counts = letter_counts(code)
-        values = set(counts.values())
-        if 2 in values:
-            n2 += 1
-        if 3 in values:
+        counts = set(Counter(code).values())
+        if 3 in counts:
             n3 += 1
+        if 2 in counts:
+            n2 += 1
+
     return n2 * n3
 
 
+def diff(code1, code2):
+    n = len(code1)
+    return n - sum(c1 == c2 for c1, c2 in zip(code1, code2) if c1 == c2)
+
+
+def part_2(codes):
+    for code1, code2 in combinations(codes, 2):
+        if diff(code1, code2) == 1:
+            return "".join(c1 for c1, c2 in zip(code1, code2) if c1 == c2)
+
+
 if __name__ == "__main__":
-    print(answer(sys.argv[1]))
+    codes = load_data(sys.argv[1])
+    print(f"Part 1: {part_1(codes)}")
+    print(f"Part 2: {part_2(codes)}")
