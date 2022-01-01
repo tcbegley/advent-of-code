606cea12dc63e8c19253bdc45e97a3454bd9954f
tcbegley <tomcbegley@gmail.com>
2022-01-01 17:58:54 +0000

    Tidy up 2018 day 1

--- 2018/day01.py
+++ 2018/day01.py
@@ -1,11 +1,29 @@
 import sys
 
 
-def answer(path):
+def load_data(path):
     with open(path) as f:
-        steps = [int(i) for i in f.read().strip().split("\n")]
+        return [int(i) for i in f.readlines()]
+
+
+def part_1(steps):
     return sum(steps)
 
 
+def part_2(steps):
+    i, n = 0, len(steps)
+    total = 0
+    seen = set()
+
+    while True:
+        if total in seen:
+            return total
+        seen.add(total)
+        total += steps[i]
+        i = (i + 1) % n
+
+
 if __name__ == "__main__":
-    print(answer(sys.argv[1]))
+    data = load_data(sys.argv[1])
+    print(f"Part 1: {part_1(data)}")
+    print(f"Part 2: {part_2(data)}")
