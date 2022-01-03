bfe6bc8a2ae104e837b2b2463e39d27069c59384
tcbegley <tomcbegley@gmail.com>
2022-01-03 12:12:59 +0000

    Tidy up 2018 day 4

--- 2018/day04.py
+++ 2018/day04.py
@@ -1,53 +1,78 @@
 import re
 import sys
-from collections import defaultdict
+from collections import Counter, defaultdict
 from datetime import datetime
 
+LOG_PATTERN = re.compile(r"\[([-: \d]+)\] (.+)")
+NUMBER_PATTERN = re.compile(r"\d+")
 
-def answer(path):
+
+def load_data(path):
+    def process_pair(pair):
+        timestamp, event = pair
+        return (
+            datetime.strptime(timestamp, "%Y-%m-%d %H:%M"),
+            event,
+        )
+
     with open(path) as f:
-        logs_raw = f.read().strip().split("\n")
+        return sorted(map(process_pair, LOG_PATTERN.findall(f.read())))
 
-    log_pattern = re.compile(r"\[(.*)\] (.*)")
-    logs = []
 
-    for log in logs_raw:
-        m = log_pattern.match(log)
-        logs.append((m.group(1), m.group(2)))
+def get_time_asleep(logs):
+    time_asleep = defaultdict(Counter)
 
-    logs = sorted(logs, key=lambda x: x[0])
-
-    asleep_time = defaultdict(lambda: {i: 0 for i in range(60)})
-
-    current_guard = None
-    asleep = False
-    start_minute, end_minute = None, None
-
-    for log in logs:
-        if log[1] == "falls asleep":
-            start_minute = int(log[0][-2:])
-        elif log[1] == "wakes up":
-            end_minute = int(log[0][-2:])
-            for i in range(start_minute, end_minute):
-                asleep_time[current_guard][i] += 1
+    current_guard, start_minute = None, None
+    for timestamp, event in logs:
+        if event == "falls asleep":
+            start_minute = timestamp.minute
+        elif event == "wakes up":
+            for minute in range(start_minute, timestamp.minute):
+                time_asleep[current_guard][minute] += 1
         else:
-            current_guard = int(re.search(r"#(\d+)", log[1]).group(1))
+            current_guard = NUMBER_PATTERN.search(event).group()
 
-    most_asleep = None
-    most_time = 0
-    for guard, times in asleep_time.items():
-        time_asleep = sum(times.values())
-        if time_asleep > most_time:
-            most_asleep = guard
-            most_time = time_asleep
+    return time_asleep
 
-    time_most_asleep = sorted(
-        [(k, v) for k, v in asleep_time[most_asleep].items()],
-        key=lambda x: x[1],
-        reverse=True,
-    )[0][0]
-    return most_asleep * time_most_asleep
+
+def part_1(logs):
+    time_asleep = get_time_asleep(logs)
+
+    most_asleep = max(
+        time_asleep, key=lambda guard: time_asleep[guard].total()
+    )
+    time_most_asleep = max(
+        time_asleep[most_asleep],
+        key=lambda minute: time_asleep[most_asleep][minute],
+    )
+    return int(most_asleep) * time_most_asleep
+
+
+def count_from_item(item):
+    _, count = item
+    return count
+
+
+def count_from_guard_item_pair(pair):
+    _, (_, count) = pair
+    return count
+
+
+def part_2(logs):
+    time_asleep = get_time_asleep(logs)
+
+    guard, (minute, _) = max(
+        (
+            (guard, max(counts.items(), key=count_from_item))
+            for guard, counts in time_asleep.items()
+        ),
+        key=count_from_guard_item_pair,
+    )
+
+    return int(guard) * minute
 
 
 if __name__ == "__main__":
-    print(answer(sys.argv[1]))
+    logs = load_data(sys.argv[1])
+    print(f"Part 1: {part_1(logs)}")
+    print(f"Part 2: {part_2(logs)}")
