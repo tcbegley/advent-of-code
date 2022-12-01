ad30b0d9f824241f2d1ddd0dbac40169d097467a
tcbegley <tomcbegley@gmail.com>
2022-12-01 21:54:42 +0000

    Tidy up 2018 day 6

--- 2018/day06.py
+++ 2018/day06.py
@@ -1,91 +1,61 @@
 import sys
-from collections import deque
-
-
-class UnionFind:
-    def __init__(self, x_range, y_range):
-        x_min, x_max = x_range
-        y_min, y_max = y_range
-
-        self.num_components = (x_max - x_min + 1) * (y_max - y_min + 1) + 1
-        self.id = {
-            (x, y): (x, y)
-            for x in range(x_min, x_max + 1)
-            for y in range(y_min, y_max + 1)
-        }
-        self.id[None] = None
-        self.size = {
-            (x, y): 1
-            for x in range(x_min, x_max + 1)
-            for y in range(y_min, y_max + 1)
-        }
-        self.size[None] = 1
-
-    def find(self, loc):
-        root = loc
-        while root != self.id[root]:
-            root = self.id[root]
-
-        # path compression
-        while loc != root:
-            next_ = self.id[loc]
-            self.id[loc] = root
-            loc = next_
-
-        return root
-
-    def component_size(self, loc):
-        return self.size[self.find(loc)]
-
-    def unify(self, loc1, loc2):
-        root1 = self.find(loc1)
-        root2 = self.find(loc2)
-
-        if root1 != root2:
-            if root2 is None or (
-                root1 is not None and self.size[root1] < self.size[root2]
-            ):
-                self.size[root2] += self.size[root1]
-                self.id[root1] = root2
-            else:
-                self.size[root1] += self.size[root2]
-                self.id[root2] = root1
-
-            self.num_components -= 1
+from collections import Counter
 
 
 def load_data(path):
     with open(path) as f:
-        return [tuple(map(int, row.split(","))) for row in f.readlines()]
+        return [
+            tuple(map(int, row.split(",")))
+            for row in f.read().strip().split("\n")
+        ]
 
 
-def get_neighbours(x, y, x_min, x_max, y_min, y_max):
-    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        if x_min <= x + dx <= x_max and y_min <= y + dy <= y_max:
-            yield x + dx, y + dy
+def compute_grid(coords):
+    xs, ys = zip(*coords)
+    min_x, max_x = min(xs), max(xs)
+    min_y, max_y = min(ys), max(ys)
+
+    # compute manhattan distance to centers for each point in the grid
+    grid = {
+        (x, y): [abs(x - cx) + abs(y - cy) for cx, cy in coords]
+        for x in range(min_x, max_x + 1)
+        for y in range(min_y, max_y)
+    }
+
+    return grid, (min_x, max_x), (min_y, max_y)
 
 
 def part_1(coords):
-    x_min, x_max = (
-        min(coords, key=lambda c: c[0])[0],
-        max(coords, key=lambda c: c[0])[0],
-    )
-    y_min, y_max = (
-        min(coords, key=lambda c: c[1])[1],
-        max(coords, key=lambda c: c[1])[1],
-    )
+    grid, (min_x, max_x), (min_y, max_y) = compute_grid(coords)
 
-    queue = deque(coords)
-    next_queue = deque()
-    visited_this_iteration = set()
-    grid = {}
+    counts = Counter()
+    disqualified = set()
 
-    while queue:
-        pass
+    for coord, dists in grid.items():
+        min_dist = min(dists)
+        if dists.count(min_dist) == 1:
+            # increment count if exactly one group acheives the min distance
+            counts[(group := dists.index(min_dist))] += 1
+            if coord[0] in (min_x, max_x) or coord[1] in (min_y, max_y):
+                # disqualify group if they claim a boundary point (in this
+                # case that group will claim infinitely many points)
+                disqualified.add(group)
+
+    for group in disqualified:
+        del counts[group]
+
+    return counts.most_common(1)[0][1]
 
 
 def part_2(coords):
-    pass
+    grid, *_ = compute_grid(coords)
+
+    count = 0
+    for dists in grid.values():
+        if sum(dists) < 10_000:
+            count += 1
+
+    return count
 
 
 if __name__ == "__main__":
