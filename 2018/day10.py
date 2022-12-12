4b4981cf922aa7769ef00d9a80816062d4bb4880
tcbegley <tomcbegley@gmail.com>
2022-12-12 07:57:25 +0000

    2018 day 10 improvement

--- 2018/day10.py
+++ 2018/day10.py
@@ -1,68 +1,76 @@
 import re
 import sys
+from dataclasses import dataclass
+
+NUMBER = re.compile(r"-?\d+")
 
 
+@dataclass
 class Particle:
-    def __init__(self, px, py, vx, vy):
-        self.px = px
-        self.py = py
-        self.vx = vx
-        self.vy = vy
+    position: tuple[int, int]
+    velocity: tuple[int, int]
 
-    def pos(self, time):
-        return (self.px + time * self.vx, self.py + time * self.vy)
+    def pos(self, t):
+        x, y = self.position
+        vx, vy = self.velocity
+        return (x + t * vx, y + t * vy)
 
 
-def answer(path):
+def load_data(path):
     with open(path) as f:
-        data = f.read().strip().split("\n")
+        data = [
+            map(int, NUMBER.findall(row))
+            for row in f.read().strip().split("\n")
+        ]
+    return [Particle((x, y), (vx, vy)) for x, y, vx, vy in data]
 
-    particles = []
-    pattern = re.compile(r"<([^<>]*)>")
-    for line in data:
-        m = pattern.findall(line)
-        px, py = [int(i) for i in m[0].split(",")]
-        vx, vy = [int(i) for i in m[1].split(",")]
-        particles.append(Particle(px, py, vx, vy))
 
-    minw = float("inf")
-    to_print = None
-    increasing = 0
-    i = 0
+def visualise(particles, t):
+    particle_lookup = {p.pos(t) for p in particles}
+    xs = [x for x, _ in particle_lookup]
+    ys = [y for _, y in particle_lookup]
 
-    while True:
-        i += 1
-        pos = [p.pos(i) for p in particles]
-        minx = min(p[0] for p in pos)
-        miny = min(p[1] for p in pos)
-        maxx = max(p[0] for p in pos)
-        maxy = max(p[1] for p in pos)
+    minx, maxx = min(xs), max(xs)
+    miny, maxy = min(ys), max(ys)
 
-        if maxx - minx < minw:
-            minw = maxx - minx
-            to_print = i
-            increasing = 0
-        else:
-            increasing += 1
+    return "\n" + "\n".join(
+        "".join(
+            "#" if (x, y) in particle_lookup else "."
+            for x in range(minx, maxx + 1)
+        )
+        for y in range(miny, maxy + 1)
+    )
 
-        if increasing > 10:
-            break
 
-    pos = [p.pos(to_print) for p in particles]
-    minx = min(p[0] for p in pos)
-    miny = min(p[1] for p in pos)
-    maxx = max(p[0] for p in pos)
-    maxy = max(p[1] for p in pos)
+def binary_search(f, low, high):
+    # find the minimum of a convex function on the integers. we use a simple
+    # finite difference to check if function is increasing or decreasing at
+    # the midpoint. If it is decreasing before and non-decreasing after then
+    # the midpoint is the minimum
+    mid = low + (high - low) // 2
+    f1, f2, f3 = f(mid - 1), f(mid), f(mid + 1)
+    if f1 > f2:
+        if f2 <= f3:
+            return mid
+        return binary_search(f, mid, high)
+    return binary_search(f, low, mid)
 
-    for y in range(miny, maxy + 1):
-        line = ""
-        for x in range(minx, maxx + 1):
-            if (x, y) in pos:
-                line += "#"
-            else:
-                line += "."
-        print(line)
+
+def solve(particles):
+    # since particles all have constant velocity, the minimum width of the
+    # particle cluster will be convex, hence we can find the time at which
+    # width is minimised with a binary search
+    def f(t):
+        xs = [p.pos(t)[0] for p in particles]
+        return max(xs) - min(xs)
+
+    # can conservatively assume that message appears in the first 1M steps
+    t = binary_search(f, 0, 1_000_000)
+    return visualise(particles, t), t
 
 
 if __name__ == "__main__":
-    answer(sys.argv[1])
+    particles = load_data(sys.argv[1])
+    message, t = solve(particles)
+    print(f"Part 1: {message}")
+    print(f"Part 2: {t}")
