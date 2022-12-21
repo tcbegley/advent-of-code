859eed0cf8f54cdc3b97e9d5ae849810b99576c6
tcbegley <tomcbegley@gmail.com>
2022-12-21 06:40:53 +0000

    2018 day 12

--- 2018/day12.py
+++ 2018/day12.py
@@ -1,34 +1,67 @@
-import re
 import sys
 
 
-def answer(path):
+def load_data(path):
     with open(path) as f:
-        lines = f.read().strip().split("\n")
+        init_state, rules = f.read().strip().split("\n\n")
+    init_state = init_state.removeprefix("initial state: ")
+    rules = dict(row.split(" => ") for row in rules.split("\n"))
+    return init_state, rules
 
-    state = f"{'.'*20}{re.search(r'[.#]+', lines[0]).group(0)}{'.'*20}"
 
-    n = len(state)
+def score(state, left):
+    return sum(i for i, p in enumerate(state, start=left) if p == "#")
 
-    rules = {}
-    for line in lines[2:]:
-        k, v = line.split(" => ")
-        rules[k] = v
 
-    for gen in range(20):
-        print(state)
-        new_state = ".."
-        for i in range(2, n - 2):
-            new_state += rules[state[i - 2 : i + 3]]
-        state = new_state + ".."
+def solve(init_state, rules, n=20):
+    # my input seems to settle down to a fixed state that moves right one
+    # space each time, but this code should work for any repeating cycle
+    state, left = init_state, 0
+    seen = {}
+    states = []
 
-    total = 0
-    for i, p in enumerate(state):
-        if p == "#":
-            total += i - 20
+    for i in range(n):
+        left += state.index("#")
+        state = state.strip(".")
 
-    return total
+        if state in seen:
+            prev_left, prev_i = seen[state]
+            # cycle_length is number of steps taken since we last saw the
+            # repeated state
+            cycle_length = i - prev_i
+            # how many cycles + remainder to reach our target
+            n_cycles, offset = divmod(n - i, cycle_length)
+            # increment the left cursor by the number of cycles times the
+            # amount the cursor moved during the last cycle, plus the offset
+            left += n_cycles * (left - prev_left) + (
+                states[prev_i + offset][1] - states[prev_i][1]
+            )
+            return score(states[prev_i + offset][0], left)
+
+        seen[state] = (left, i)
+        states.append((state, left))
+
+        left -= 4
+        state = f"....{state}...."
+
+        left += 2
+        state = "".join(
+            rules.get(state[i - 2 : i + 3], ".")
+            for i in range(2, len(state) - 2)
+        )
+
+    return score(state, left)
+
+
+def part_1(init_state, rules):
+    return solve(init_state, rules)
+
+
+def part_2(init_state, rules):
+    return solve(init_state, rules, 50_000_000_000)
 
 
 if __name__ == "__main__":
-    print(answer(sys.argv[1]))
+    init_state, rules = load_data(sys.argv[1])
+    print(f"Part 1: {part_1(init_state, rules)}")
+    print(f"Part 2: {part_2(init_state, rules)}")
