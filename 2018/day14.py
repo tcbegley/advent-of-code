82eae216b479ae9ed50f7cbbc56b50d92484b7cf
tcbegley <tomcbegley@gmail.com>
2022-12-23 21:33:15 +0000

    2018 day 14

--- 2018/day14.py
+++ 2018/day14.py
@@ -1,26 +1,52 @@
 import sys
 
 
-class Elf:
-    def __init__(self, loc, recipe):
-        self.loc = loc
-        self.recipe = recipe
+def load_data(path):
+    with open(path) as f:
+        return int(f.read().strip())
 
 
-def answer(n):
+def digits(n):
+    for d in str(n):
+        yield int(d)
+
+
+def solve(hook):
+    elf1, elf2 = 0, 1
     recipes = [3, 7]
-    elf1 = Elf(0, 3)
-    elf2 = Elf(1, 7)
 
-    while len(recipes) < n + 10:
-        recipes.extend([int(i) for i in str(elf1.recipe + elf2.recipe)])
-        elf1.loc = (elf1.loc + 1 + elf1.recipe) % len(recipes)
-        elf1.recipe = recipes[elf1.loc]
-        elf2.loc = (elf2.loc + 1 + elf2.recipe) % len(recipes)
-        elf2.recipe = recipes[elf2.loc]
+    while True:
+        recipes.extend(digits(recipes[elf1] + recipes[elf2]))
+        elf1 = (elf1 + recipes[elf1] + 1) % len(recipes)
+        elf2 = (elf2 + recipes[elf2] + 1) % len(recipes)
 
-    return "".join(str(i) for i in recipes[n : n + 10])
+        if (res := hook(recipes)) is not None:
+            return res
+
+
+def part_1(n):
+    def hook(recipes):
+        if len(recipes) < n + 10:
+            return None
+        return "".join(map(str, recipes[n : n + 10]))
+
+    return solve(hook)
+
+
+def part_2(n):
+    target = list(digits(n))
+
+    def hook(recipes):
+        if target == recipes[-6:]:
+            return len(recipes) - 6
+        elif target == recipes[-7:-1]:
+            return len(recipes) - 7
+        return None
+
+    return solve(hook)
 
 
 if __name__ == "__main__":
-    print(answer(int(sys.argv[1])))
+    n = load_data(sys.argv[1])
+    print(f"Part 1: {part_1(n)}")
+    print(f"Part 2: {part_2(n)}")
