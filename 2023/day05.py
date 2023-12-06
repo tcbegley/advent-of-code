eb5defd072831fca95807b1381cb1219a7c028bc
tcbegley <tomcbegley@gmail.com>
2023-12-06 22:33:47 +0000

    wip

--- 2023/day05.py
+++ 2023/day05.py
@@ -1,41 +1,113 @@
 import sys
 from dataclasses import dataclass
 from tqdm.auto import tqdm
+from collections import deque
 
 
-@dataclass
-class MapEntry:
-    dest: int
-    src: int
-    length: int
+@dataclass(frozen=True)
+class Interval:
+    low: int
+    high: int
+    offset: int
 
 
 def parse_block(block):
     _, *block = block.split("\n")
-    return [MapEntry(*map(int, row.split(" "))) for row in block]
+    maps = []
+    for row in block:
+        dest, src, length = map(int, row.split(" "))
+        maps.append(Interval(src, src + length, dest - src))
+    return sorted(maps, key=lambda m: m.low)
 
 
 def load_data(path):
     with open(path) as f:
         seeds, *blocks = f.read().strip().split("\n\n")
-    seeds = list(map(int, seeds.split(":")[1].strip().split(" ")))
+    seeds = list(map(int, seeds.removeprefix("seeds:").strip().split(" ")))
     maps = [parse_block(block) for block in blocks]
-    return seeds, maps
+    return seeds, maps[::-1]
+
+
+def map_intervals(intervals, maps):
+    intervals = deque(intervals)
+    maps = deque(maps)
+
+    mapped_intervals = []
+    while maps:
+        map_ = maps.popleft()
+
+        while True:
+            interval = intervals.popleft()
+            if interval.high <= map_.low:
+                # intervals don't overlap
+                mapped_intervals.append(interval)
+            elif interval.high <= map_.high:
+                # intervals overlap, but map_ extends beyond interval
+                if interval.low < map_.low:
+                    mapped_intervals.append(
+                        Interval(interval.low, map_.low, interval.offset)
+                    )
+                    mapped_intervals.append(
+                        Interval(
+                            map_.low,
+                            interval.high,
+                            interval.offset + map_.offset,
+                        )
+                    )
+                else:
+                    mapped_intervals.append(
+                        Interval(
+                            interval.low,
+                            interval.high,
+                            interval.offset + map_.offset,
+                        )
+                    )
+            else:
+                # intervals overlap, interval extends beyond map
+                if interval.low < map_.low:
+                    mapped_intervals.append(
+                        Interval(interval.low, map_.low, interval.offset)
+                    )
+                    mapped_intervals.append(
+                        Interval(
+                            map_.low,
+                            map_.high,
+                            interval.offset + map_.offset,
+                        )
+                    )
+                else:
+                    mapped_intervals.append(
+                        Interval(
+                            interval.low,
+                            map_.high,
+                            interval.offset + map_.offset,
+                        )
+                    )
+                intervals.appendleft(
+                    Interval(map_.high, interval.high, interval.offset)
+                )
+                break
+
+    mapped_intervals.extend(intervals)
+
+    return mapped_intervals
+
+
+def map_seed(seed, intervals):
+    for interval in intervals:
+        if interval.low <= seed < interval.high:
+            return seed + interval.offset
+
+    raise RuntimeError
 
 
 def part_1(seeds, maps):
-    numbers = seeds
-    for map_ in maps:
-        new_numbers = []
-        for number in tqdm(numbers):
-            for entry in map_:
-                if entry.src <= number < entry.src + entry.length:
-                    new_numbers.append(entry.dest + (number - entry.src))
-                    break
-            else:
-                new_numbers.append(number)
-        numbers = new_numbers
-    return min(numbers)
+    intervals = [Interval(float("-inf"), float("inf"), 0)]
+
+    for m in maps:
+        intervals = map_intervals(intervals, m)
+
+    return [map_seed(seed, intervals) for seed in seeds]
 
 
 def part_2(seeds, maps):
@@ -49,4 +121,4 @@ def part_2(seeds, maps):
 if __name__ == "__main__":
     seeds, maps = load_data(sys.argv[1])
     print(f"Part 1: {part_1(seeds, maps)}")
-    print(f"Part 2: {part_2(seeds, maps)}")
+    # print(f"Part 2: {part_2(seeds, maps)}")
