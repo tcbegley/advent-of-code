250ed8c423a4d422cc29f705a1c7b2327e57fb07
Tom Begley <tomcbegley@gmail.com>
2024-12-17 15:01:45 +0000

    2018 day 21

---
+++ 2018/day21alt.py
@@ -0,0 +1,77 @@
+import sys
+
+
+def part_1_alt():
+    r2 = r5 = 0
+    count = 0
+    while True:
+        print(f"{r2=}, {r5=}")
+        r5 = r2 | 2**16
+
+        r2 = 4843319 + r5 & (2**8 - 1)
+        r2 &= 16777215
+        r2 *= 65899
+        r2 &= 16777215
+
+        # assert 256 > r5
+        if r2 == 0 or count > 1000:
+            break
+        count += 1
+
+
+OPS = {
+    "addr": lambda a, b, r: r[a] + r[b],
+    "addi": lambda a, b, r: r[a] + b,
+    "mulr": lambda a, b, r: r[a] * r[b],
+    "muli": lambda a, b, r: r[a] * b,
+    "banr": lambda a, b, r: r[a] & r[b],
+    "bani": lambda a, b, r: r[a] & b,
+    "borr": lambda a, b, r: r[a] | r[b],
+    "bori": lambda a, b, r: r[a] | b,
+    "setr": lambda a, b, r: r[a],
+    "seti": lambda a, b, r: a,
+    "gtir": lambda a, b, r: 1 if a > r[b] else 0,
+    "gtri": lambda a, b, r: 1 if r[a] > b else 0,
+    "gtrr": lambda a, b, r: 1 if r[a] > r[b] else 0,
+    "eqir": lambda a, b, r: 1 if a == r[b] else 0,
+    "eqri": lambda a, b, r: 1 if r[a] == b else 0,
+    "eqrr": lambda a, b, r: 1 if r[a] == r[b] else 0,
+}
+
+
+def load_data(path):
+    with open(path) as f:
+        instructions = [row.split(" ", 1) for row in f.read().strip().split("\n")]
+
+    instructions = [
+        (cmd, tuple(map(int, args.split(" ")))) for cmd, args in instructions
+    ]
+    instructions[0] = (instructions[0][0], instructions[0][1][0])
+    return instructions
+
+
+def part_1(data):
+    (cmd, idx), *data = data
+    registers = [0] * 6
+
+    steps = 0
+    ip = registers[idx]
+
+    while steps < 1000:
+        print(registers)
+        registers[idx] = ip
+        cmd, (a, b, c) = data[ip]
+        registers[c] = OPS[cmd](a, b, registers)
+        ip = registers[idx]
+        ip += 1
+        steps += 1
+
+
+def part_2(data):
+    pass
+
+
+if __name__ == "__main__":
+    data = load_data(sys.argv[1])
+    print(f"Part 1: {part_1_alt()}")
+    print(f"Part 2: {part_2(data)}")
